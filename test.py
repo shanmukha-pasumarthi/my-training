db1b09cb3f3f9ab62637e5e8ca611d2b600ed84f
shanmukha <shanmukhaaa@gmail.com>
2025-07-29 14:41:49 +0530

    3rd commit

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-assasADD
-dafsaf
-dsfdsfdsfs
+# add_two_numbers.py
+
+# Get input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
 
